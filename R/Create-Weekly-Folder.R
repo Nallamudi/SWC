8f36b4fb515d33839f59c1fce8b5bf208dab4d91
Sarath Nallamudi <sarath@streetwisecapital.net>
2023-04-02 19:01:02 -0400

    Weekly 4/2/2023

--- R/Create-Weekly-Folder.R
+++ R/Create-Weekly-Folder.R
@@ -6,6 +6,6 @@ Weekly_Folder <- paste0(Content_Folder, format(Sys.Date(),"%Y-%m-%d"),"_Weekly-M
 
 create_directory(Weekly_Folder)
 
-file.copy(paste0(here::here(),"/R/index.qmd"), paste0(Weekly_Folder, "index.qmd"))
+file.copy(paste0(here::here(),"/R/Weekly_Yahoo/index.qmd"), paste0(Weekly_Folder, "index.qmd"))
 #dir.copy(paste0(here::here(),"/R/index.md"), paste0(Weekly_Folder, "index.markdown_strict_files"))
 
